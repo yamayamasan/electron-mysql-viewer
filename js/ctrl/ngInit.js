e4d70308dfd25415168ea43d281174e9d12f02dd
suzuki <sliker.kiley@gmail.com>
2016-10-20 16:57:10 +0900

    Delete file

--- js/ctrl/ngInit.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('mysql', ['ui.router']);
-
-function AppEmitter() {
-  EventEmitter.call(this);
-}
-util.inherits(AppEmitter, EventEmitter);
-const appEmitter = new AppEmitter();
